a3010d80d1af4e41ed43f44bf243ea3a7a8f8568
Noah Thompson <noah1010@me.com>
2020-07-14 21:01:40 -0700

    Added full plane sounds movement and fixed camera

---
+++ Plane Game/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public float offset;
+    float rotationDamping = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = 15.0f;
+    }
+
+    // Update is called once per frame
+
+    void LateUpdate()
+    {
+        if (!plane)
+            return;
+
+        float wantedRotationAngleSide = plane.transform.eulerAngles.y;
+        float currentRotationAngleSide = transform.eulerAngles.y;
+
+        float wantedRotationAngleUp = plane.transform.eulerAngles.x;
+        float currentRotationAngleUp = transform.eulerAngles.x;
+
+        currentRotationAngleSide = Mathf.LerpAngle(currentRotationAngleSide, wantedRotationAngleSide, rotationDamping * Time.deltaTime);
+
+        currentRotationAngleUp = Mathf.LerpAngle(currentRotationAngleUp, wantedRotationAngleUp, rotationDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(currentRotationAngleUp, currentRotationAngleSide, 0);
+
+        transform.position = plane.transform.position;
+        transform.position -= currentRotation * Vector3.forward * offset;
+
+        transform.LookAt(plane.transform);
+
+        transform.position += transform.up;
+    }
+}
---
+++ Plane Game/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rollInput;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float horizantalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1.42f;
+        rotationSpeed = 24;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's input
+        verticalInput = Input.GetAxis("Vertical");
+        horizantalInput = Input.GetAxis("Horizontal");
+        rollInput = Input.GetAxis("Roll");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed* verticalInput * Time.deltaTime);
+
+        // tilt plane right and left based on Input
+        transform.Rotate(Vector3.up * rotationSpeed * horizantalInput * Time.deltaTime);
+
+        // roll plane right and left based on Input
+        transform.Rotate(Vector3.forward * rotationSpeed * -rollInput * Time.deltaTime);
+
+    }
+}
